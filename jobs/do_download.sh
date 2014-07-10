57d23327aede733644a4d2cc17141b085bd8f337
Jason Stajich <jason@bioperl.org>
2014-07-09 22:24:29 -0700

    jobs for the queueing

---
+++ jobs/do_download.sh
@@ -0,0 +1,4 @@
+module load perl
+
+perl scripts/download_genbank_eutil.pl lib/accessions.csv &> download.log
+
---
+++ jobs/do_update.sh
@@ -0,0 +1,3 @@
+module load perl
+
+perl scripts/add_accessions.pl > lib/accessions_update.csv
