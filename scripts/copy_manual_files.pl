9ea83ca5637385c548ed85e0ea90051229f6439c
Jason Stajich <jason@bioperl.org>
2015-05-07 00:02:23 -0700

    some improvements to validate against organisms file

--- scripts/copy_manual_files.pl
+++ scripts/copy_manual_files.pl
@@ -1,10 +1,15 @@
 #!env perl
 use strict;
 use warnings;
+use Getopt::Long;
+use Text::CSV_XS;
 my $debug = 1;
 my $debug_one = 0;
 my $force = 0;
+
+GetOptions('force!' => \$force);
 my $dir = shift || "../manual_genomes/download";
+my $genome_file = shift || 'lib/organisms.csv';
 my $p_odir = shift || 'final_combine/pep';
 my $g_odir = shift || 'final_combine/GFF';
 my $d_odir = shift || 'final_combine/DNA';
@@ -14,6 +19,31 @@ mkdir("final_combine") unless -d "final_combine";
 for my $d ( $p_odir, $g_odir, $d_odir, $c_odir ) {
  mkdir($d) unless -d $d;
 }
+
+my $csv = Text::CSV_XS->new ({ binary => 1, auto_diag => 1 });
+open my $fh, "<:encoding(utf8)", $genome_file or die "$genome_file: $!";
+
+my %orgs;
+my %jgi_targets;
+while (my $row = $csv->getline ($fh)) {
+    next if( $row->[0] =~ /^\#/);
+    next unless( $row->[3] =~ /^manual/ );
+    warn("name is ",$row->[0], "\n");
+
+    if ( ! defined $row->[3] ) {
+	warn(join(",",@$row),"\n");
+	next;
+    }
+    my $stem = $row->[0];
+    $stem .= "_".$row->[1] if $row->[1];
+    $stem =~ s/ /_/g;;
+    $orgs{$stem} = { 
+	'name'   => $row->[0],
+	'strain' => $row->[1],
+	'family' => $row->[2],
+	'source' => $row->[3] };
+}
+
 opendir(DIR, $dir) || die $!;
 for my $f ( readdir(DIR) ) {
     next if $f =~ /^\./;
@@ -25,6 +55,10 @@ for my $f ( readdir(DIR) ) {
     for my $sp ( readdir(FAM) ) {
 	next if $sp =~ /^\./;
 	next if ( ! -d "$dir/$f/$sp");
+	if( ! $orgs{$sp} ) {
+	    warn("$sp ($f) not found in target $genome_file, skipping\n");
+	    next;
+	}
 #	warn("sp is $dir/$f/$sp\n") if $debug;
 	opendir(SP,"$dir/$f/$sp") || die "$dir/$f/$sp $!";
 	for my $file ( readdir(SP) ) {
@@ -34,21 +68,23 @@ for my $f ( readdir(DIR) ) {
 		my $pref = &make_prefix($2);
 		warn "AA stem is $stem pref is $pref\n";
 		next if -f "$p_odir/$stem";
-		
-		print("zcat $dir/$f/$sp/$file | perl -p -e 's/>/>$pref|/' > $p_odir/$stem\n");
+		#print("zcat $dir/$f/$sp/$file | perl -p -e 's/>/>$pref|/' > $p_odir/$stem\n");
+		`zcat $dir/$f/$sp/$file | perl -p -e 's/>/>$pref|/' > $p_odir/$stem`;
             } elsif ( $file =~ /((\S+)\.CDS\.fasta).gz$/) {
 		my $stem = $1;
 		my $pref = &make_prefix($2);
 		warn "CDS stem is $stem pref is $pref\n";
                 next if -f "$c_odir/$stem";
-		print("zcat $dir/$f/$sp/$file | perl -p -e 's/>/>$pref|/' > $c_odir/$stem\n");
+		`zcat $dir/$f/$sp/$file | perl -p -e 's/>/>$pref|/' > $c_odir/$stem`;
+		#print ("zcat $dir/$f/$sp/$file | perl -p -e 's/>/>$pref|/' > $c_odir/$stem\n");
 	    } elsif ( $file =~ /((\S+)\.assembly\.fasta).gz$/) {
 		my $stem = $1;
 		my $pref = &make_prefix($2);
 		$stem =~ s/\.assembly//;
 		warn "ASM stem is $stem pref is $pref\n";
 		next if -f "$d_odir/$stem";
-		print("zcat $dir/$f/$sp/$file |  perl -p -e 's/>/>$pref|/' > $d_odir/$stem\n");
+		#print("zcat $dir/$f/$sp/$file |  perl -p -e 's/>/>$pref|/' > $d_odir/$stem\n");
+		`zcat $dir/$f/$sp/$file |  perl -p -e 's/>/>$pref|/' > $d_odir/$stem`;
 	    } elsif ( $file =~ /(\S+\.gff3).gz$/) {
 		my $stem = $1;
 		warn "GFF stem is $stem\n";
@@ -61,6 +97,7 @@ for my $f ( readdir(DIR) ) {
 			chomp;
 			my @row = split(/\t/,$_);
 			my $last = pop @row;
+			$last =~ s/;\s*$//;
 			my (@order,%ninth);
 			for my $ent ( split(/;/,$last) ) {
 			    my ($id,$val) = split(/=/,$ent);
@@ -89,7 +126,7 @@ sub make_prefix {
     my @parts = split(/\./,$stem);
     my ($genus,$species) = split(/_/,$parts[0]);
     my $pref;
-    if( @parts > 1 ) {
+    if( @parts > 1 && $stem !~ /M1\.001/) { # hacky special case but I'm not sure how else to make this smarter
         $pref = $parts[1];
     } else {
         $pref = substr($genus,0,1).substr($species,0,3);
